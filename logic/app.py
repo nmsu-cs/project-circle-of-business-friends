d0142024442237c27e7530f3e717e788538cd52d
carsonSiegrist <carsonsiegrist@gmail.com>
2024-03-28 15:12:40 -0600

    Added temporary logo to navbar, set up static folder

--- logic/app.py
+++ logic/app.py
@@ -5,7 +5,7 @@ from profile_logic import profile_bp
 from user_portal_logic import user_portal_bp
 from index_logic import index_bp
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder="../static")
 app.secret_key = '123123'
 app.register_blueprint(signup_bp)
 app.register_blueprint(login_bp)
