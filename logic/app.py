28adfdb1d013facd6e27b5f5b3f061e1d3da4e42
JD <jonahlepinski@gmail.com>
2024-03-29 10:09:14 -0600

    Home page
    Implements a home page that displays user profiles using a javascript function and allows the user to like/dislike the profile.
    
    These profiles come from a list returned from profilearray.py.
    
    Right now, the logic doesn't exist for us to loop through the actual userids in the database (see TODO in profilearray.py) so the array must be manually changed to reflect what user ids are in your database.
    
    There is a function call when a profile is liked/disliked that currently does nothing. Need to implement some kind of logic there.
    
    Adds home to navbar

--- logic/app.py
+++ logic/app.py
@@ -4,14 +4,17 @@ from login_logic import login_bp
 from profile_logic import profile_bp
 from user_portal_logic import user_portal_bp
 from index_logic import index_bp
+from profilearray import profilearray_bp
 
 app = Flask(__name__, template_folder='../templates', static_folder="../static")
+
 app.secret_key = '123123'
 app.register_blueprint(signup_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(profile_bp)
 app.register_blueprint(user_portal_bp)
 app.register_blueprint(index_bp)
+app.register_blueprint(profilearray_bp)
 
 @app.route("/login")
 def login():
@@ -27,6 +30,11 @@ def signup():
 def landing_page():
     title = "Welcome"
     return render_template("landing_page.html", title=title)
+
+@app.route("/home")
+def home():
+    title = "Home"
+    return render_template("home.html", title=title)
 
 
 
