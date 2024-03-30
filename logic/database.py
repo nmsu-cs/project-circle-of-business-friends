adafc7b559410442a3aa2f0e8e7bfaaff5a88d39
dcarr001 <dcarr001@nmsu.edu>
2024-03-30 03:51:00 +0000

    added check so database doesn't get recreated each time it is accessed(just in case) and adjusted table attributes

--- logic/database.py
+++ logic/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Float, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Float, ForeignKey, JSON
 from sqlalchemy.orm import relationship, declarative_base
 import configparser
 import os
@@ -12,48 +12,55 @@ db_path = os.path.expandvars(db_path)
 if not db_path:
     raise ValueError("DATABASE_PATH environment variable is not set")
 
-engine = create_engine(f'sqlite:///{db_path}', echo=True)
+engine = create_engine(f'sqlite:///{db_path}', echo=False)
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    
-    profile = relationship('Profile', uselist =False, back_populates='user') 
-    matches = relationship('Match', foreign_keys='Match.user_id')
+        __tablename__ = 'users'
+        id = Column(Integer, primary_key=True)
+        username = Column(String, unique=True, nullable=False)
+        email = Column(String, unique=True, nullable=False)
+        password = Column(String, nullable=False)
+        
+        profile = relationship('Profile', uselist =False, back_populates='user') 
+        matches = relationship('Match', foreign_keys='Match.user_id')
 
 class Profile(Base):
     __tablename__ = 'profiles'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     firstName = Column(String, nullable=False)
     lastName = Column(String, nullable=False)
     age = Column(Integer)
     gender = Column(String)
-    interests = Column(String)
+    interests = Column(JSON)
     occupation = Column(String)
     education_level = Column(String)
     major = Column(String)
+    #This value is determined by interests attribute and is assigned in profile_logic.py
+    vmagnitude = Column(Integer)
 
     user = relationship('User', back_populates='profile')
+
 
 class Match(Base):
     __tablename__ = 'matches'
     match_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    matched_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    matched_user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     compatibility_score = Column(Float)
-    
-    
-Base.metadata.create_all(engine)
 
-metadata = MetaData()
-metadata.reflect(bind=engine)
 
-if 'users' in metadata.tables and 'matches' in metadata.tables and 'profiles' in metadata.tables:
-    print("SUCCESS")
+if not os.path.exists(db_path):
+
+    print("DATABASE NOT CREATED. CREATING NOW...") 
+    Base.metadata.create_all(engine)
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+
+    if 'users' in metadata.tables and 'matches' in metadata.tables and 'profiles' in metadata.tables:
+        print("SUCCESS")
+    else:
+        print('FAILURE')
 else:
-    print('FAILURE')
+    print("DATABASE INITALIZED")
