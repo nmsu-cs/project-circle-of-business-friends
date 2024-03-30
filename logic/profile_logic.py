ced43bebb74ae4cb45957f6e7e1c6563dec6d03a
dcarr001 <dcarr001@nmsu.edu>
2024-03-30 03:52:43 +0000

    interests is now set list, adjusted insertion to database, and added matching functionality to populate Match table.

--- logic/profile_logic.py
+++ logic/profile_logic.py
@@ -1,11 +1,26 @@
 from flask import render_template, request, redirect, url_for, flash, session, Blueprint
 from sqlalchemy.orm import sessionmaker
 from database import engine, Profile
+from collections import defaultdict
+from matching import calc_magnitude, update_matches
 
 profile_bp = Blueprint('profile', __name__)
 
 # Connect to database via SQLAlchemy
 Session = sessionmaker(bind=engine)
+
+#Interest definitions
+INTERESTS_LIST = [
+    'football',
+    'basketball',
+    'soccer',
+    'art',
+    'music',
+    'food',
+    'bar-hopping',
+    'partying',
+    'dinner-ing?'
+]
 
 @profile_bp.route('/profile', methods=['GET', 'POST'])
 def profile():
@@ -19,23 +34,34 @@ def profile():
     if request.method == 'POST':
         age = request.form.get('age')
         gender = request.form.get('gender')
-        interests = request.form.get('interests')
+        interests = request.form.get('selected_interests').split(",")
         occupation = request.form.get('occupation')
         education_level = request.form.get('education_level')
         major = request.form.get('major')
 
+
+
         if user:
+            i_dict = defaultdict(int)
+            for i in interests:
+                i_dict[i] = 1
+
             user.age = age
             user.gender = gender
-            user.interests = interests
+            user.interests = i_dict
             user.occupation = occupation
             user.education_level = education_level
             user.major = major
+            user.vmagnitude = calc_magnitude(i_dict)
 
             sqlsession.commit()
             flash('Profile updated successfully')
+
+            #Update matches table
+            update_matches(sqlsession, user)
+
             sqlsession.close()
             return redirect(url_for('user_portal.user_portal'))
         else:
             messages.append('User not found')
-    return render_template('profile.html', messages=messages, user=user)
+    return render_template('profile.html', messages=messages, user=user, interests=INTERESTS_LIST)
