5f4e5724d7d151373bea776865ec8ac759ddeee7
dcarr001 <dcarr001@nmsu.edu>
2024-03-30 03:49:57 +0000

    added retrieval of top matches to pass to html

--- logic/user_portal_logic.py
+++ logic/user_portal_logic.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, session, Blueprint
 from sqlalchemy.orm import sessionmaker
 from database import engine, Profile
+from matching import get_matches
 
 user_portal_bp = Blueprint('user_portal', __name__)
 
@@ -17,8 +18,12 @@ def user_portal():
         #Retrieve user
         user_id = session['user_id']
         user_profile = sqlsession.query(Profile).filter_by(id=user_id).first()
+
+        #Retrieve top matches, change last variable to increase display count
+        top_matches = get_matches(sqlsession, user_id, 5)
+
         sqlsession.close()
 
-        return render_template('user_portal.html', user=user_profile)
+        return render_template('user_portal.html', user=user_profile, top_matches=top_matches)
     else:
         return redirect(url_for('login.login'))
