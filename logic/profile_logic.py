21caa9eca5d6cb322ce13f92faeab9167af94796
dcarr001 <dcarr001@nmsu.edu>
2024-03-31 06:56:51 +0000

    updated profile logic to pass more info to profile.html

--- logic/profile_logic.py
+++ logic/profile_logic.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for, flash, session, B
 from sqlalchemy.orm import sessionmaker
 from database import engine, Profile
 from collections import defaultdict
-from matching import calc_magnitude, update_matches
+from matching import update_matches
 
 profile_bp = Blueprint('profile', __name__)
 
@@ -22,6 +22,31 @@ INTERESTS_LIST = [
     'dinner-ing?'
 ]
 
+MAJOR_LIST = [
+    'majorA',
+    'majorB',
+    'majorC',
+    'majorD',
+    'majorE',
+    'majorF'
+]
+
+OCC_LIST = [
+    'occA',
+    'occB',
+    'occC',
+    'occD',
+    'occE',
+    'occF'
+]
+
+ED_LIST = [
+    'Freshman',
+    'Sophomore',
+    'Junior',
+    'Senior'
+]
+
 @profile_bp.route('/profile', methods=['GET', 'POST'])
 def profile():
     
@@ -30,30 +55,31 @@ def profile():
     messages = []
     
     user = sqlsession.query(Profile).filter_by(user_id=user_id).first()
+    interests_string = ""
 
     if request.method == 'POST':
         age = request.form.get('age')
         gender = request.form.get('gender')
-        interests = request.form.get('selected_interests').split(",") #turn string into list
+        interests = request.form.get('selected_interests') #turn string into list
         occupation = request.form.get('occupation')
         education_level = request.form.get('education_level')
         major = request.form.get('major')
 
-
+        interests_string = interests
+        interests_list = interests.split(",")
 
         if user:
             #convert string to list for use in cosine similarity function
             i_dict = defaultdict(int)
-            for i in interests:
+            for i in interests_list:
                 i_dict[i] = 1
-
+            
             user.age = age
             user.gender = gender
             user.interests = i_dict
             user.occupation = occupation
             user.education_level = education_level
             user.major = major
-            user.vmagnitude = calc_magnitude(i_dict) #calculate vector magnitude based on interests
 
             sqlsession.commit()
             flash('Profile updated successfully')
@@ -65,4 +91,6 @@ def profile():
             return redirect(url_for('user_portal.user_portal'))
         else:
             messages.append('User not found')
-    return render_template('profile.html', messages=messages, user=user, interests=INTERESTS_LIST)
+
+        
+    return render_template('profile.html', messages=messages, user=user, interests_string=interests_string, interests=INTERESTS_LIST, majors=MAJOR_LIST, occupations=OCC_LIST, ed_level=ED_LIST)
