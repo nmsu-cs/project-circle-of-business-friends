41520d9e67e1fafc2048c4a32e7ba94f2f54c99c
dcarr001 <dcarr001@nmsu.edu>
2024-03-31 08:56:01 +0000

    added username string to pass to html

--- logic/user_portal_logic.py
+++ logic/user_portal_logic.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, session, Blueprint
 from sqlalchemy.orm import sessionmaker
-from database import engine, Profile
+from database import engine, Profile, User
 from matching import get_matches
 
 user_portal_bp = Blueprint('user_portal', __name__)
@@ -17,13 +17,15 @@ def user_portal():
     
         #Retrieve user
         user_id = session['user_id']
-        user_profile = sqlsession.query(Profile).filter_by(id=user_id).first()
+        user_profile = sqlsession.query(Profile).filter_by(user_id=user_id).first()
+        user = sqlsession.query(User).filter_by(id=user_id).first()
+        username = user.username
 
         #Retrieve top matches, change last variable to increase display count
         top_matches = get_matches(sqlsession, user_id, 5)
 
         sqlsession.close()
 
-        return render_template('user_portal.html', user=user_profile, top_matches=top_matches)
+        return render_template('user_portal.html', user=user_profile, top_matches=top_matches, username=username)
     else:
         return redirect(url_for('login.login'))
