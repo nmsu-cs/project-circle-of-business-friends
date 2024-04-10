9bb4b9eb51d8137146c2f0b8a640b2e08ce90b84
dcarr001 <dcarr001@nmsu.edu>
2024-04-10 07:10:11 +0000

    signup logic to talk to flask and looks pretty

--- frontend/NMSUandMe/src/pages/signup.vue
+++ frontend/NMSUandMe/src/pages/signup.vue
@@ -1,16 +1,171 @@
 <template>
-    <v-container class="fill-height">
+    <v-container>
     <v-responsive
       class="align-centerfill-height mx-auto"
       max-width="900"
     >
 
+      <v-alert
+      v-if="showAlert"
+      color="error"
+      icon="$error"
+      title="Error"
+      :text="alertMessage"
+      dismissible
+      ></v-alert>
+
       <div class="text-center">
-        <h1 class="text-h2 font-weight-bold">SignUp</h1>
+        <h1 class="text-h2 font-weight-bold">Sign Up</h1>
       </div>
 
       <div class="py-4" />
 
+      <form @submit.prevent="submit">
+        <v-text-field
+          v-model="username.value.value"
+          :counter="10"
+          :error-messages="username.errorMessage.value"
+          label="Username"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email.value.value"
+          :error-messages="email.errorMessage.value"
+          label="E-mail"
+        ></v-text-field>
+
+        <v-text-field
+          :append-inner-icon="eyeIcon"
+          :type="inputType"
+          v-model="password.value.value"
+          :counter="12"
+          :error-messages="password.errorMessage.value"
+          label="Password"
+          @click:append-inner="toggleVis"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="firstName.value.value"
+          :counter="2"
+          :error-messages="firstName.errorMessage.value"
+          label="First Name"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="lastName.value.value"
+          :counter="2"
+          :error-messages="lastName.errorMessage.value"
+          label="Last Name"
+        ></v-text-field>
+
+        <v-checkbox
+          v-model="checkbox.value.value"
+          :error-messages="checkbox.errorMessage.value"
+          label="T&C"
+          type="checkbox"
+          value="1"
+        ></v-checkbox>
+
+        <v-btn
+          class="me-4" type="submit"> submit </v-btn>
+
+        <v-btn @click="handleReset"> clear </v-btn>
+      </form>
+
     </v-responsive>
   </v-container>
+    
 </template>
+
+<script setup>
+  import { ref, computed } from 'vue'
+  import { useField, useForm } from 'vee-validate'
+  import {useRouter} from 'vue-router'
+  import axios from 'axios'
+
+  const router = useRouter()
+
+  const visible = ref(true)
+  const showAlert = ref(false)
+  const alertMessage = ref('')
+  const path = 'http://localhost:5000/signup'
+
+  const showErrorMessage = (message) => {
+    alertMessage.value = message
+    showAlert.value = true
+  }
+
+  const toggleVis = () => {visible.value = !visible.value};
+  const inputType = computed(() => visible.value ? 'text' : 'password');
+  const eyeIcon = computed(() => visible.value ? 'mdi-eye' : 'mdi-eye-off');
+
+
+  const { handleSubmit, handleReset } = useForm({
+    validationSchema: {
+      username : async (value) => {
+        if(value?.length >= 10) return true
+        
+        return "Username needs to be at least 10 characters"
+      },
+      password (value) {
+        if (value?.length > 12 && /(?=.*[@#$%^&*!~])/.test(value)) return true
+
+        return 'Password must be at least 12 characters long and contain 1 special character.'
+      },
+      email (value) {
+        if (/^[a-z0-9.-]+@nmsu.edu$/i.test(value)) return true
+
+        return 'Must be a valid e-mail.'
+      },
+      firstName (value) {
+        if (value?.length >= 2) return true
+
+        return 'Name needs to be at least 2 characters.'
+      },
+      lastName (value) {
+        if (value?.length >= 2) return true
+
+        return 'Name needs to be at least 2 characters.'
+      },
+      checkbox (value) {
+        if (value === '1') return true
+
+        return 'Must be checked.'
+      },
+    },
+  })
+  const username = useField('username')
+  const password = useField('password')
+  const email = useField('email')
+  const firstName = useField('firstName')
+  const lastName = useField('lastName')
+  const select = useField('select')
+  const checkbox = useField('checkbox')
+
+  const submit = handleSubmit( async values => {
+    try {
+        const response = await axios.post(path, values)
+        console.log(response.data)
+        if(response.data.status === 'success'){
+          console.log('User added:', response.data.msg)
+          router.push('/profile')
+        }
+
+        else if(response.data.msg === 'error'){
+            console.log('Conflict found:', response.data.msg)
+            showErrorMessage(response.data.msg)
+        }
+        else{
+          console.log('Failed to add user:', response.data.msg)
+          showErrorMessage(response.data.msg)
+        }
+      }
+
+      catch (error){
+        console.error('Error adding user:', error.message)
+        showErrorMessage(response.data.msg)
+      }
+  })
+
+
+</script>
