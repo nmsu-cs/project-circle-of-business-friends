ddfa88400d92016340f901c11bc1eff5772388fb
dcarr001 <dcarr001@nmsu.edu>
2024-04-10 07:09:48 +0000

    created temp page for profile

---
+++ frontend/NMSUandMe/src/pages/profile.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+
+      <div class="text-center">
+        <h1 class="text-h2 font-weight-bold">Profile</h1>
+      </div>
+
+
+      <div class="py-4" />
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+</script>
