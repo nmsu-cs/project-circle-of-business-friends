df3e70bed623ce016d6b9dbf3f606b58316e12e1
dcarr001 <dcarr001@nmsu.edu>
2024-04-19 22:40:38 +0000

    added necessary routes

--- frontend/NMSUandMe/src/router/index.js
+++ frontend/NMSUandMe/src/router/index.js
@@ -9,7 +9,8 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import index from '../pages/index.vue'
 import signup from '../pages/signup.vue'
-import profile from '../pages/signup.vue'
+import profile from '../pages/profile.vue'
+import user_portal from '../pages/user_portal.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,11 @@ const router = createRouter({
       path:'/profile',
       name: profile,
       component: profile
+    },
+    {
+      path: '/user_portal',
+      name: user_portal,
+      component: user_portal
     }
   ]
 })
