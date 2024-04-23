1a2049e7bdbfedf11f041c370ed77f1459ddb9c4
dcarr001 <dcarr001@nmsu.edu>
2024-04-23 19:55:08 +0000

    updated user_portal logic to pass profile info

--- backend/user_portal_logic.py
+++ backend/user_portal_logic.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, session, Blueprint
+from flask import jsonify, Blueprint, request
 from sqlalchemy.orm import sessionmaker
 from database import engine, Profile, User
 from matching import get_matches
@@ -7,45 +7,35 @@ from collections import defaultdict
 
 user_portal_bp = Blueprint('user_portal', __name__)
 
-#global variables
-CURRENT_INDEX=-1
-
 # Connect to database via SQLAlchemy
 Session = sessionmaker(bind=engine)
 
 @user_portal_bp.route('/user_portal')
 def user_portal():
     sqlsession = Session()
+    response_object = {'status':'success'}
 
-    #Check if user is logged in
-    if 'user_id' in session:
-    
-        #Retrieve user
-        user_id = session['user_id']
-        user_profile = sqlsession.query(Profile).filter_by(user_id=user_id).first()
-        user = sqlsession.query(User).filter_by(id=user_id).first()
-        username = user.username
+    if request.method == "GET":
+            user_id = request.args.get('user_id')
 
-        #Retrieve top matches, change last variable to increase display count
-        top_matches = get_matches(sqlsession, user_id, 5)
+            if not user_id:
+                response_object['status'] = 'error'
+                response_object['msg'] = 'user_id param is missing'
+                return jsonify(response_object)
+            
+            try:
+                user_profile = sqlsession.query(Profile).filter_by(user_id=user_id).first()
+                user = sqlsession.query(User).filter_by(id=user_id).first()
 
-        matches, ids = profile_convert(top_matches)
-        matches_json = json.dumps(matches)
-        ids_json = json.dumps(ids)
-        sqlsession.close()
+                response_object['data']={
+                     "username":user.username,
+                     "firstName":user_profile.firstName,
+                     "lastName":user_profile.lastName
+                }
 
-        return render_template('user_portal.html', user=user_profile, username=username, matches = matches_json, ids=ids_json)
-    else:
-        return redirect(url_for('login.login'))
-    
-def profile_convert(matches):
-    ret = defaultdict(list)
-    ids = []
-    for match, profile in matches:
-        ret[match.match_id]=[
-            profile.firstName,
-            profile.lastName,
-            match.compatibility_score
-        ]
-        ids.append(match.match_id)
-    return ret, ids
+                return jsonify(response_object)
+            
+            except ValueError:
+                response_object['status'] = 'error'
+                response_object['msg'] = 'Invalid user_id'
+                return jsonify(response_object)
