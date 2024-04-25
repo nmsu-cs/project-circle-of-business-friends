68671c494f2c7f48680b9aff47806137831a4240
carsonSiegrist <carsonsiegrist@gmail.com>
2024-04-24 22:25:07 -0600

    Removed extraneous library import

--- backend/event_scraper.py
+++ backend/event_scraper.py
@@ -8,7 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
-import requests
 import os
 
 #Options for chrome UPDATE if you change the webdriver
