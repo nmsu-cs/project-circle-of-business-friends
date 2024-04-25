a63f2eec309be5ca830a8394d2937167c2a4fae0
dcarr001 <dcarr001@nmsu.edu>
2024-04-25 20:50:57 +0000

    updated matches display

--- frontend/NMSUandMe/src/pages/matches.vue
+++ frontend/NMSUandMe/src/pages/matches.vue
@@ -1,45 +1,54 @@
 <template>
-    <userPortalLayout
-      :profileClicked="goToProfile"
-      :matchesClicked="goToUserPortal"
-      :logoutClicked="logout"
-    >
-      <template #buttons>
-        <v-btn @click="goToProfile">Profile</v-btn>
-        <v-btn @click="goToUserPortal">User Portal</v-btn>
-        <v-btn @click="logout">Logout</v-btn>
-      </template>
-      
-      <v-container class="text-center">
-        <h1>Welcome to Matches</h1>
-      </v-container>
-      <v-container class="text-center">
-        <v-carousel>
-            <v-carousel-item v-for="(match, index) in matches" :key="index">
-                <v-row align="center" class="fill-height">
-                    <v-col cols="12">
-                        <v-card class="mx-auto" max-width="500">
-                            <v-card-title>
-                            {{ match[0] }} {{ match[1] }}
-                            </v-card-title>
-                            <v-card-text>
-                            <p>Score: {{ match[2] }}</p>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-container class="text-center">
-                                    <v-btn @click="">Confirm</v-btn>
-                                    <v-btn class="ml-4" @click="">Skip</v-btn>
-                                </v-container>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-carousel-item>
-        </v-carousel>
-      </v-container>
-      
-    </userPortalLayout>
-  </template>
+  <userPortalLayout :userPortalClicked="goToUserPortal" :logoutClicked="logout">
+    <template #buttons>
+      <v-btn @click="goToUserPortal">User Portal</v-btn>
+      <v-btn @click="logout">Logout</v-btn>
+    </template>
+
+    <v-container class="text-center">
+      <h1>Welcome to Matches</h1>
+    </v-container>
+    <v-container class="text-center" v-if="matches">
+      <v-carousel hide-delimiters v-model="carosul_model">
+        <v-carousel-item v-for="(match, index) in matches" :key="index">
+          <v-row align="center" class="fill-height">
+            <v-col cols="12">
+              <v-card class="mx-auto" max-width="500">
+                <v-card-title>
+                  {{ match[0] }} {{ match[1] }}
+                </v-card-title>
+                <v-card-text>
+                  <p>Score: {{ match[6] }}</p>
+                  <p>Major: {{ match[2] }}</p>
+                  <p>Year: {{ match[3] }}</p>
+                  <p>Career Interest: {{ match[4]}}</p>
+                  <p>Interests:</p>
+                </v-card-text>
+                <v-chip v-for="(interest, index) in match[5]" :key="index" class="ma-1" outlined>
+                  {{ interest }}
+                </v-chip>
+                <v-card-actions>
+                  <v-container class="text-center">
+                    <v-btn @click="">Confirm</v-btn>
+                    <v-btn class="ml-4" @click="">Skip</v-btn>
+                  </v-container>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <v-container class="text-center" v-else>
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <v-progress-circular indeterminate size="64"></v-progress-circular>
+        </v-col>
+      </v-row>
+    </v-container>
+    e
+  </userPortalLayout>
+</template>
   
   <script setup>
   import userPortalLayout from '@/layouts/userPortalLayout.vue'
@@ -51,6 +60,7 @@
   const path = "http://localhost:5000/matches"
 
   const matches = ref({})
+  const carosul_model = ref(0)
 
   onMounted(() => {
     user_id_temp.value = router.currentRoute.value.query.user_id
@@ -65,18 +75,13 @@
     })
     })
   
-  const goToProfile = () => {
-    const user_id = user_id_temp.value
-    router.push({path:'/profile', query: {user_id},})
-  }
-  
   const goToUserPortal = () => {
     const id=user_id_temp.value
     router.push({path:'/user_portal', query: {id},})
   }
   
   const logout = () => {
-    // Your logout logic here
+    router.push('/')
   }
   </script>
   
